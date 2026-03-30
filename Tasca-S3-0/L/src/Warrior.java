7201f177453197ae10e06614457e241194daf01f
AlessandroPesole <alespesole@gmail.com>
2026-03-30 13:30:33 +0200

    feat: assign standard physical damage behavior to Warrior

--- Tasca-S3-0/L/src/Warrior.java
+++ Tasca-S3-0/L/src/Warrior.java
@@ -1,11 +1,11 @@
 public class Warrior extends Character {
+    public Warrior() {
+        super(new StandardPhysicalDamage());
+    }
+
     @Override
     public void attack() {
         System.out.println("The warrior strikes with a sword.");
     }
 
-    @Override
-    public void takeDamage(int points) {
-        System.out.println("The warrior resists and only takes " + (points / 2) + " points of damage.");
-    }
 }
