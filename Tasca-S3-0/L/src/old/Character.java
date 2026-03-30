462699b27b28bc92f67dfe2e9fc20f5ada9aea48
AlessandroPesole <alespesole@gmail.com>
2026-03-30 11:54:14 +0200

    feat: copy of old classes

---
+++ Tasca-S3-0/L/src/old/Character.java
@@ -0,0 +1,11 @@
+package old;
+
+public class Character {
+    public void attack() {
+        System.out.println("The character attacks with a weapon.");
+    }
+
+    public void takeDamage(int points) {
+        System.out.println("The character takes " + points + " points of damage.");
+    }
+}
