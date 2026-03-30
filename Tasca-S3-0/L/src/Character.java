d295e76d3c863cc5c30df96161a807da54a79748
AlessandroPesole <alespesole@gmail.com>
2026-03-30 13:31:31 +0200

    feat: refactor Character to use new damage strategy

--- Tasca-S3-0/L/src/Character.java
+++ Tasca-S3-0/L/src/Character.java
@@ -1,9 +1,15 @@
 public class Character {
+    PhysicalDamageable physicalDamageable;
+
+    public Character(PhysicalDamageable physicalDamageable) {
+        this.physicalDamageable = physicalDamageable;
+    }
+
     public void attack() {
         System.out.println("The character attacks with a weapon.");
     }
 
     public void takeDamage(int points) {
-        System.out.println("The character takes " + points + " points of damage.");
+        physicalDamageable.takeDamage(points);
     }
 }
